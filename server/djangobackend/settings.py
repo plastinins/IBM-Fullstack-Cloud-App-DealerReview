2caa6ea35cac8fb20c76b3705f02554ba378ba4d
plastinins <plastinin@yandex.ru>
2021-04-14 19:15:45 +0000

    added page templates for about and contacts

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'ao5z(o(z@cvzodm99d32jkxa5e8a1!q_4sqss5-a%n6tg$#h$+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["dealerreview.eu-gb.mybluemix.net",
+                "localhost"]
 
 
 # Application definition
