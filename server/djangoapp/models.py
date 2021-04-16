379d866094d9a50743aa6909eace1b352340bd75
plastinins <plastinin@yandex.ru>
2021-04-16 15:37:12 +0000

    Adding proxy for dealerreview API

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -52,12 +52,37 @@ class CarModel(models.Model):
     year = models.DateField()
 
     def __str__(self):
-        return "Make: " + self.make.name + ", " + \
-               "Model: " + self.name + ", " + \
+        return self.make.name + " " + \
+               self.name + ", " + \
                "Year: " + str(self.year.year) + ", " + \
                "DealerID: " + str(self.dealer_id)
 
-# <HINT> Create a plain Python class `CarDealer` to hold dealer data
+class CarDealer:
+    def __init__(self, address, city, full_name, id, lat, long, short_name, state, st, zip):
+        # Dealer address
+        self.address = address
+        # Dealer city
+        self.city = city
+        # Dealer Full Name
+        self.full_name = full_name
+        # Dealer id
+        self.id = id
+        # Location lat
+        self.lat = lat
+        # Location long
+        self.long = long
+        # Dealer short name
+        self.short_name = short_name
+        # Dealer state
+        self.state = state
+        # Dealer state (short)
+        self.st = st
+        # Dealer zip
+        self.zip = zip
+
+    def __str__(self):
+        return "Dealer name: " + self.full_name
+
 
 
 # <HINT> Create a plain Python class `DealerReview` to hold review data
